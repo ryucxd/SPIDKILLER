778dec5eb238618c471ffcf55ff2c38a038b93a7
ryucxd <ryucxd@gmail.com>
2022-08-30 10:58:47 +0100

    Add project files.

---
+++ SPIDKILLER/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace SPIDKILLER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTableName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnKill = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTableName
+            // 
+            this.txtTableName.Location = new System.Drawing.Point(93, 62);
+            this.txtTableName.Name = "txtTableName";
+            this.txtTableName.Size = new System.Drawing.Size(245, 20);
+            this.txtTableName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter Table Name";
+            // 
+            // btnKill
+            // 
+            this.btnKill.Location = new System.Drawing.Point(263, 88);
+            this.btnKill.Name = "btnKill";
+            this.btnKill.Size = new System.Drawing.Size(75, 23);
+            this.btnKill.TabIndex = 2;
+            this.btnKill.Text = "Kill";
+            this.btnKill.UseVisualStyleBackColor = true;
+            this.btnKill.Click += new System.EventHandler(this.btnKill_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 225);
+            this.Controls.Add(this.btnKill);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTableName);
+            this.Name = "Form1";
+            this.Text = "SPID KILLLLLLLLERRRR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTableName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnKill;
+    }
+}
+
---
+++ SPIDKILLER/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SPIDKILLER
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKill_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                SqlConnection conn = new SqlConnection(Connect.ConnectionString);
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("spidkiller", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+
+                cmd.Parameters.AddWithValue("TNAME", txtTableName.Text);
+
+                cmd.ExecuteScalar();
+
+                conn.Close();
+                MessageBox.Show("Table locks for table " + txtTableName.Text + " now released!" , "Success", MessageBoxButtons.OK);
+
+
+
+            }
+            catch
+            {
+                MessageBox.Show("An error has occured, please check the table name", "Error", MessageBoxButtons.OK);
+            }
+
+        }
+    }
+}
